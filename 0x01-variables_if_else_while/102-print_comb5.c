b8f00a627dd627b6f346890bba972f33813decea
sara <saramustafa184@gmail.com>
2023-08-12 00:57:56 -0700

    task done in c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - programe to print combinatons of two two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+	int p;
+
+	for (n == 45; n <= 9; n++)
+	{
+		for (m == 45; m <= 9; m++)
+		{
+			for (l == 45; l <= 9; l++)
+			{
+				for (p == 46; p <= 9; p++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					putchar(p);
+				}
+			}
+		}
+	}
+	return (0);
+}
