ec8e6de00166aed52ca62799ad7160e19accb1e5
sara <saramustafa184@gmail.com>
2023-08-15 15:25:36 -0700

    task done in c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integer number
+ * @i: numer one
+ * @j: number twi
+ * Return: add of two numbers
+ */
+int add(int i, int j)
+{
+	int m;
+
+	m = i + j;
+	_putchar(m);
+}
+
