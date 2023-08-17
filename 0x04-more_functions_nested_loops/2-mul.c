5da881959cab043cc09573b96d97f3825ea79e2c
sara <saramustafa184@gmail.com>
2023-08-17 05:25:02 -0700

    task done in c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplication two number
+ *@a: iput number ineger
+ *@b: input number integr
+ *Return: mult product
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
