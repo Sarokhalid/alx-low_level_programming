94716760b49a96343723a7f727339389ab9fd83f
sara <saramustafa184@gmail.com>
2023-08-17 08:09:59 -0700

    task done in c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - print in digonal \
+ * @n: number input
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			if (i == j)
+				_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
