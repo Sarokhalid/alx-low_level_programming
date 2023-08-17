959c616699e7994e496cea9fb4cb28e552b745e8
sara <saramustafa184@gmail.com>
2023-08-17 12:00:52 -0700

    task done in c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,7 +15,10 @@ void print_square(int size)
 		{
 			for (j = 0; j <= size; j++)
 			{
-				_putchar('#');
+				if ((i + j) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void print_square(int size);
 void print_diagonal(int n);
 void print_line(int n);
@@ -6,7 +7,6 @@ void print_most_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
-int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
