21fbfb2d7f5138065f592f1ebec67af6e8b5e347
sara <saramustafa184@gmail.com>
2023-08-17 07:39:15 -0700

    task done in c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: number of times the _ character print
+ */
+void print_line(int n)
+{
+	if (n <= o)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
