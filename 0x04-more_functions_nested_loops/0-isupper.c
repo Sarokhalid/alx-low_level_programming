f92258ce2cfd4af30a458149687e542466fd479f
sara <saramustafa184@gmail.com>
2023-08-17 03:24:24 -0700

    task done in c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - check the alphabet character
+ * @c: char Ascii code
+ * Return: 1 if c is uppercase .0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{	_putchar(c);
+		c++;
+		return (1);
+	}
+	else
+		return (0);
+}
