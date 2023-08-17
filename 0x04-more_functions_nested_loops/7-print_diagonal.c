75f0a147687dfe34cb5af9ff2bf29febca8c7ab0
sara <saramustafa184@gmail.com>
2023-08-17 08:24:07 -0700

    task done in c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,21 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 		_putchar('\n');
-	int i;
-	int j;
-
-	for (i = 1; i <= n; i++)
+	else
 	{
-		for (j = 1; j <= n; j++)
-		{
-			if (i == j)
-				_putchar('\\');
+		int i;
+		int j;
+
+		for (i = 1; i <= n; i++)
+		{	
+			for (j = 1; j <= n; j++)
+			{
+				if (i == j)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
