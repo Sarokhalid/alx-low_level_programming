9654301b4b7c4437d5abadadf9dbb63004f34243
sara <saramustafa184@gmail.com>
2023-08-17 05:06:01 -0700

    task done in c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isdigit - chech digit
- * @c: c is input digit
+ * @c: c is char 
  * Return: 1 if c is digit. 0 if not
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
