2a4d7b95c184b1e489e0d0cb4100e7eabcbbcf9e
sara <saramustafa184@gmail.com>
2023-08-21 04:46:01 -0700

    task done in c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
 void _puts(char *str);
+void puts_half(char *str);
 void rev_string(char *s);
 
 void reset_to_98(int *n);
