886139982c439675b5b4f9798a2e05646652a2f1
sara <saramustafa184@gmail.com>
2023-08-21 04:56:35 -0700

    task done in c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void print_rev(char *s);
 void _puts(char *str);
 void puts_half(char *str);
 void rev_string(char *s);
+void print_array(int *a, int n);
 
 void reset_to_98(int *n);
 int _putchar(char c);
