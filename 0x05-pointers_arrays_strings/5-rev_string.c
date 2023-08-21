9f45290674e89dd7bb584d951f8f04fd820ac1c0
sara <saramustafa184@gmail.com>
2023-08-21 03:42:42 -0700

    task done in c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - print string in reverse
+ * @s: character of parameter
+ */
+void rev_string(char *s)
+{
+	int x, g;
+	char t;
+
+	for (x = 0; *s != '\0'; s++)
+		;
+	for (g = 0; g < x / 2; g++)
+	{
+		t = s[i];
+		s[i] = s[x - 1 - g];
+		s[x - 1 - g] = t;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
 void _puts(char *str);
+void rev_string(char *s);
 
 void reset_to_98(int *n);
 int _putchar(char c);
