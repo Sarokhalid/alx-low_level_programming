cb9155f37845293c034dec8af98fb79eec35f70e
sara <saramustafa184@gmail.com>
2023-08-23 03:50:01 -0700

    task done in c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - to upper
+ * @str: string
+ * @Return: value
+ */
+char *string_toupper(char *str)
+{
+	int t;
+
+	for (t = 0; str[t] != '\0'; t++)
+	{
+		if (str[t] >= 97 && str[t] <= 122)
+			str[t] = str[t] - 32;
+	}
+	return (str);
+}
+
