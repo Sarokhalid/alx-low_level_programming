683ac78f8d85f46e3c15be82db02dfc04605ad5e
sara <saramustafa184@gmail.com>
2023-08-23 04:12:15 -0700

    task done in c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main h"
+/**
+ * leet - encoding
+ * @c: string
+ * Return: value
+ */
+char *leet(char *c)
+{
+	char *p = c;
+	char k[] = {'A', 'E', 'O', 'T', 'L'};
+	int v[] = {4, 3, 0, 7,1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(k) /sizeof(char); i++)
+		{
+			if (*c == k[i] || *c == k[i] +32)
+				*c = 48 +v[i];
+		}
+		c++;
+	}
+	return (p);
+}
+
