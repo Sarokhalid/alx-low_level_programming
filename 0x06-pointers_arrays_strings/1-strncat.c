bd405b252261c60d0707f1da980c15225a6d421c
sara <saramustafa184@gmail.com>
2023-08-23 02:43:56 -0700

    task done in c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - print concatenate
+ * @dest: string of charracter
+ * @src: string of character
+ * @n: ineger
+ * Return: concatenate value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int b, m;
+
+	while (dest[b])
+		b++;
+	for (m = 0; m < n && src[m] != '\0'; m++)
+		dest[b + m] = src[m];
+	dest[c + i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 int _putchar(int c);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strcat(char *dest, char *src);
