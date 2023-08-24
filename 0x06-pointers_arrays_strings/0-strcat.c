c6f33d72acb3d179ee2ebf050b8b6f49f21ea207
sara <saramustafa184@gmail.com>
2023-08-23 23:10:59 -0700

    task done in c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,13 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int z, x = 0;
-	
-	while (src[z]) != '\0')
+	int z, x;
+
+	z = 0;
+	while (dest[z] != '\0')
+		z++;
+	x = 0;
+	while (src[x] != '\0')
 	{
-		dest[x] = src[z];
+		dest[z] = src[x];
 		z++;
 		x++;
 	}
+	dest[z] = '\0';
 	return (dest);
 }
