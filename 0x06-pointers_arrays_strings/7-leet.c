9697403210ab4ef5e3e26bd32f41f5d7f4f96a3c
sara <saramustafa184@gmail.com>
2023-08-24 01:02:12 -0700

    task done in c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,11 +10,11 @@ char *leet(char *n)
 	char w1[] = "aAeEoOtTlL";
 	char w2[] = "4433007711";
 
-	for (i = 0; n[i] != 0; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] = w1[j]
+			if (n[i] == w1[j])
 			{
 				n[i] = w2[j]
 			}
