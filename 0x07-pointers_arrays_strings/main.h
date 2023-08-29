7ee6de79721ac726594e5f7638eada4696bc91ae
sara <saramustafa184@gmail.com>
2023-08-28 20:17:34 -0700

    task done in c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char _putchar(char c);
+void set_string(char **s, char *to);
 
