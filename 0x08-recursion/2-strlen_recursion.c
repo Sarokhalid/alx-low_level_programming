7b0bc5c38bdda5dd1532a30b3d409ce0a95e9833
sara <saramustafa184@gmail.com>
2023-08-30 06:31:06 -0700

    task done in c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print lenght of string
+ * @s: string
+ * Return: lenght of string
+ */
+int _strlen_recursion(char *s);
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
