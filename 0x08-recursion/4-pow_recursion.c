2e3fd6f857612fcad63451a74338eeca4e0939cb
sara <saramustafa184@gmail.com>
2023-08-30 07:30:45 -0700

    task done in c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - print piwer
+ * @x: input
+ * @y: powee
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
