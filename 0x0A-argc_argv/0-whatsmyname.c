1c315b2cd744de306e9ef2302d4ccc000edc0747
sara <saramustafa184@gmail.com>
2023-09-01 13:40:49 -0700

    task done in c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - hffxzz
+ * @argc: hnjj
+ * @argv: hjnk
+ * Return: 0
+ */
+int main(int argc, char const argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
