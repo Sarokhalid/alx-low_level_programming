5331a4757032ed0661273884249319d5b74501e8
sara <saramustafa184@gmail.com>
2023-09-01 17:43:58 -0700

    task done in c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - ghjg
- * @argc: gjjj
- * @argv: ghjj
- * Return: 0
+ * main -  gjj
+ * @argc: hjhvc
+ * @argv: vnn
+ * Return: bjvc
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+void (argv);
+	printf("%d\n", argc - 1);
 	return (0);
 }
