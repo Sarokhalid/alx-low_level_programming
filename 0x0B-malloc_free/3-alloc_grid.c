d9cdceb28360aecec47f8e335e22e51d39c8c7f0
sara <saramustafa184@gmail.com>
2023-09-04 04:22:28 -0700

    task done in c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - hgcc
+ * @width: nvc
+ * @height: vccvbn
+ * Return: bcxv
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	g = (int **)malloc(height * sizeof(int **));
+	if (g == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		g[i] = (int *)malloc(width * sizeof(int));
+		if (g[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(g[i]);
+			free(g);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			g[i][j] = 0;
+	}
+	return (g);
+}
