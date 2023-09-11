7c1491f1cf30fe15e2019f32a21b1d8ed7bbca66
sara <saramustafa184@gmail.com>
2023-09-11 12:28:17 -0700

    task done in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: vcxx
  * @owner: jgcxz
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} Doogs;
 typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
