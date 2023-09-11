6f09168a6d0c0f3d44670413c2a9c850fb14f6e8
sara <saramustafa184@gmail.com>
2023-09-11 12:25:33 -0700

    task done in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,20 +12,7 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * struct dog - Represent dog with its name,age  and owner
- * @name: name of dog
- * @age : age of dog
- * @owner: owner of dog
- * Description: this struct represent name and age and owner of dog
- */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} Doogs;
-typedef Doogs dog_t;
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
