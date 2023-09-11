26e8ed2c9daa6df77167794cca00744ddfda5b49
sara <saramustafa184@gmail.com>
2023-09-11 07:58:02 -0700

    task done in c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOOGS_H
+#define DOOGS_H
+/**
+ * struct dog - Represent dog with its name,age  and owner
+ * @name: name of dog
+ * @age : age of dog
+ * @owner: owner of dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} Doogs;
+#endif
