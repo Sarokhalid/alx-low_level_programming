764763d529624b51817257510c1d59ffa0481311
sara <saramustafa184@gmail.com>
2023-09-11 08:28:39 -0700

    task done in c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#ifndef DOGS_H
+#define DOGS_H
+/**
+ * init_dog - bvcx
+ * @d: nkhg
+ * @name: bvcc
+ * @age: bbvcc
+ * @owner: bbvc
+ * Return: bcc
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+#endif
