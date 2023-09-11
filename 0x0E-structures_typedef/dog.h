20e60187b4c107a31545363ac2c42ccd92eb25a7
sara <saramustafa184@gmail.com>
2023-09-11 12:39:20 -0700

    task done in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,25 @@
-#ifndef DOOGS_H
-#define DOOGS_H
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - jgfdsz
- * @name: nvcx
- * @age: vcxx
- * @owner: jgcxz
+ * struct dog - declare dog
+ * @name: name of did
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: the struct dog represent the nameand age and owner of dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} Doogs;
-typedef dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
+};
+/**
+ * dog_t - typdef
+ */
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
+
