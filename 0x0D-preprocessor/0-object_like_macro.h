0bc2cb7669443f068e949efb1e8d9306b9f4a755
sara <saramustafa184@gmail.com>
2023-09-11 19:57:03 -0700

    task done in c

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 #define SIZE 1024
+#include <unistd.h>
+int _putchar(char c);
 #endif /* OBJECT_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - bfdss
+ * @c: jhfdd
+ * Return: jhfds
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
