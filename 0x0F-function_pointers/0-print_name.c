1ff34f587fa67107b0ce36f068b91c4430b752c8
sara <saramustafa184@gmail.com>
2023-09-13 02:55:45 -0700

    task done in c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - jgfdx
  * @name: nvfxz
@@ -7,14 +8,22 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
 /**
  * pname - jgfxx
  * @name: jhgfcxx
  */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
 void pname(char *name)
 {
+	if (name == NULL)
+		return;
 	while (*name != '\0')
 	{
 		_putchar(*name);
