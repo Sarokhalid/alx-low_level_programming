1748635dae609efea7cdcb2def63f3adc05dec8b
sara <saramustafa184@gmail.com>
2023-09-13 03:51:32 -0700

    task done in c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "includefunction_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - bvdsshj
+ * @size: nvcx
+ * @action: nhgccc
+ * @array: bccds
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t s;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (s = 0; s < size; s++)
+		action(array[s]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
+void array_ iterator(int *array, size_t size, void (*action)(int));
 
-void print_name(char *name, void (*f)(char *));
+aoid print_name(char *name, void (*f)(char *));
 #endif
