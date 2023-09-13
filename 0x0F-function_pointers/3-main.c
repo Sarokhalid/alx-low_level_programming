1b3a105f90a09876f8ed797596b120302e62494f
sara <saramustafa184@gmail.com>
2023-09-13 09:01:42 -0700

    task done in c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "3-calc.h"
+/**
+ * _putchar - bvxdx
+ * @c: bgcx
+ * Return: bvxx
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * main - hfdd
+ * @argc: ngcc
+ * @argv: nvcx
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int n, m, r;
+	int (*op_f)(int, int);
+
+	if (argc != 4)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+	}
+	n = atoi(argv[1]);
+	m = atoi(argv[3]);
+
+	op_f = get_op_func(argv[2]);
+
+	if (op_f == NULL)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && m == 0)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+	}
+	r = op_f(n, m);
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: int
+ * Return: sub of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: int
+ * @b: int
+ * Return: mul a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div
+ * @a: int
+ * @b: int
+ * Return: div of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: int
+ * @b: int
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
