d38bd3e2938564ae8b7930236ec98cb7f617b0dd
sara <saramustafa1842002@gmail.com>
2023-09-21 07:13:30 -0700

    task done in c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,4 @@
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- * Description: singly linked list node structure
- */
-typdef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-/**
- * print_list - hdhghshf
- * @h: jjdhgeh
- * Return: kjdjjhjd
- */
-
+#include "lists.h"
 size_t print_list(const list_t *h)
 {
 	size_t c = 0;
@@ -27,7 +9,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u} %s\n", h->len, h->str);
-		h = h->naxt;
+		h = h->next;
 		c++;
 	}
 	return (c);
