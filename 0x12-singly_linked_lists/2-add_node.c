745222253a01c85c0a71b387fa11aeb6fb441ce3
sara <saramustafa1842002@gmail.com>
2023-09-21 08:32:51 -0700

    task done in c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,10 +7,11 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *nnode;
+
 	if (str == NULL)
 		return (NULL);
-	list_t *nnode = (list_t *)malloc(sizeof(list_t));
-
+	nnode = (list_t *)malloc(sizeof(list_t));
 	if (nnode == NULL)
 		return (NULL);
 	nnode->str = strdup(str);
