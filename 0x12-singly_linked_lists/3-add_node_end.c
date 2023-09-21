c9fab1c14324c9f39ffb0bf2552067520027173f
sara <saramustafa1842002@gmail.com>
2023-09-21 08:56:38 -0700

    task done in c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_node_end - hfcx
+ * @head: bbvcc
+ * @str: bvcccx
+ * Return: bbccc
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nnode;
+
+	if (str == NULL)
+		return (NULL);
+	nnode = (list_t *)malloc(sizeof(list_t));
+	if (nnode == NULL)
+		return (NULL);
+	nnode->str = strdup(str);
+	if (nnode->str == NULL)
+	{
+		free(nnode);
+		return (NULL);
+	}
+	nnode->len = strlen(str);
+	nnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = nnode;
+	}
+	else
+	{
+		list_t *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = nnode;
+	}
+	return (nnode);
+}
+
