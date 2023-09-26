830d82e60ed5ec6b2fd3f68f2b68987895ccd242
sara <saramustafa1842002@gmail.com>
2023-09-26 02:56:02 -0700

    task done in c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - hvcxx
+ * @head: hvcx
+ * @n: bvvvc
+ * Return: hgcxx
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nn = malloc(sizeof(listint_t));
+
+	if (nn == NULL)
+		return (NULL);
+	nn->n = n;
+	nn->next = NULL;
+	if (*head == NULL)
+		*heade = nn;
+	else
+	{
+		listint_t *c == *head;
+
+		while (c->next != NULL)
+			c = c->next;
+		c->next = nn;
+	}
+	return (nn);
+}
+
