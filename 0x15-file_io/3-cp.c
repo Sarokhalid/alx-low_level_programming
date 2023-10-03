0e992c558f66eba301fdf88258215856190e06fe
sara <saramustafa1842002@gmail.com>
2023-10-03 01:04:18 -0700

    task done in c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,49 +1,54 @@
 #include "main.h"
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	const char *fm;
+	const char *ft;
+	int fdf, fdt;
+	char buffer[BUFFER_SIZE];
+	ssize_t br, bw;
+
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-	int fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	fm = argv[1];
+	ft = argv[2];
+	fdf = open(fm, O_RDONLY);
+	if (fdf == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(2, "Error: Can't read from file %s\n", fm);
 		exit(98);
 	}
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd_to == -1)
+	fdt = open(ft, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fdt == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
+		dprintf(2, "Error: Can't write to %s\n", ft);
 		exit(99);
 	}
-	char buffer[BUFFER_SIZE = 1024];
-	ssize_t bytes_read, bytes_written;
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((br = read(fdf, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		bw = write(fdt, buffer, br);
+		if (bw == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
+			dprintf(2, "Error: Can't write to %s\n", ft);
 			exit(99);
 		}
 	}
-	if (bytes_read == -1)
+	if (br == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(2, "Error: Can't read from file %s\n", fm);
 		exit(98);
 	}
-	if (close(fd_from) )
+	if (close(fdf))
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		dprintf(2, "Error: Can't close fd %d\n", fdf);
 		exit(100);
 	}
-	if (close(fd_to) == -1)
+	if (close(fdt) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		dprintf(2, "Error: Can't close fd %d\n", fdt);
 		exit(100);
 	}
-	return 0;
+	return (0);
 }
