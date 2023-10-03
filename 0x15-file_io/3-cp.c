91fb1a586f8697efdbe5508d6cc19941d8bb6f0e
sara <saramustafa1842002@gmail.com>
2023-10-03 10:13:08 -0700

    task done in c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,8 @@
 #define ERR_NOREDA "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
-#define PERMISSIONS (S_IWUSER | S_IWUSER | S_IRGRP | S_IWGRP | S_IROTH)
+#define PERMISSIONS (S_IWUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define READ_BUF_SIZE 1024
 /**
  * main - kfjjfhj
  * @ac: jdjhhd
@@ -20,25 +21,21 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-	{
-		print_error(strerror(errno));
-		return (98);
-		fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-		if (fd_to == -1)
-			dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-		to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-		if (to_fd == -1)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2], exit(99);
+			dprintf(STDERR_FILENO, ERR_NOREDA, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-		while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
-				if (write(to_fd, buf, b) != b)
-					dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-		if (b == -1)
-			dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-		from_fd = close(from_fd);
-		to_fd = close(to_fd);
-		if (from_fd)
-			dprintf(STDERR_FILENO, ERR_NOCLOSE, frpm_fd), exit(100);
-		if (to_fd)
-			dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
-		return (EXIT_SUCCESS);
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREDA, av[1]), exit(98);
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	return (EXIT_SUCCESS);
+}
