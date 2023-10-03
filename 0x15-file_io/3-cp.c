b7a85889c6cfb49e3a33d416ffa02d5d74956088
sara <saramustafa1842002@gmail.com>
2023-10-03 08:24:24 -0700

    task done in c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,72 @@
 #include "main.h"
+/**
+ * print_error - kjdqdhefh
+ * @message: mndjf
+ */
+void print_error(const char *message)
+{
+	dprintf(STDERR_FILENO, "Error: %s\n", message);
+}
+/**
+ * main - kfjjfhj
+ * @argc: jdjhhd
+ * @argv: kdjjhfd
+ */
 int main(int argc, char *argv[])
 {
-	const char *fm;
-	const char *ft;
-	int fdf, fdt;
-	char buffer[BUFFER_SIZE];
-	ssize_t br, bw;
+	
+	 const char *file_from = argv[1];
+	 const char *file_to = argv[2];
+	 char buffer[BUFFER_SIZE];
+	 int fd_to, fd_from;
+	 ssize_t br, bw;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		print_error("Usage: cp file_from file_to");
+		return (97);
 	}
-	fm = argv[1];
-	ft = argv[2];
-	fdf = open(fm, O_RDONLY);
-	if (fdf == -1)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", fm); exit(98);
-	}
-	fdt = open(ft, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fdt == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", ft); exit(99);
-	}
-	while ((br = read(fdf, buffer, BUFFER_SIZE)) > 0)
-	{
-		bw = write(fdt, buffer, br);
-		if (bw == -1)
+		print_error(strerror(errno));
+		return (98);
+		fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (fd_to == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", ft); exit(99);
+			print_error(strerror(errno));
+			close(fd_from);
+			return (99);
 		}
-	}
-	if (br == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", fm); exit(98);
-	}
-	if (close(fdf))
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fdf); exit(100);
-	}
-	if (close(fdt) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fdt); exit(100);
+		while ((br = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+		{
+			bw = write(fd_to, buffer, br);
+			if (bw == -1)
+			{
+				print_error(strerror(errno));
+				close(fd_from);
+				close(fd_to);
+				return (99);
+			}
+		}
+		if (br == -1)
+		{
+			print_error(strerror(errno));
+			close(fd_from);
+			close(fd_to);
+			return (99);
+		}
+		if (close(fd_from) == -1)
+		{
+			print_error(strerror(errno));
+			close(fd_to);
+			return (100);
+		}
+		if (close(fd_to) == -1)
+		{
+			print_error(strerror(errno));
+			return (100);
+		}
 	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 #define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
