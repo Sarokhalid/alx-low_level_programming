c50653563bd646e0441325cd1817c8f0415f4fe7
sara <saramustafa1842002@gmail.com>
2023-11-09 01:33:03 -0800

    task done in c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stddef>
+#include "lists.h"
+/**
+ * free_dlistint - hgfdx
+ * @head: jgfxx
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
