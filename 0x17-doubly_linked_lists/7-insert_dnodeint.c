950f2ba097245587ec9c3370d759c4f21542d5d1
sara <saramustafa1842002@gmail.com>
2023-11-09 10:13:51 -0800

    task done in c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  * insert_dnodeint_at_index - hgfds
+  * @h: jvcc
+  * @idx: nvvcx
+  * @n: nvvccvh
+  * Return: nbccx
+  */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current = *h;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->prev = current;
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	return (new_node);
+}
