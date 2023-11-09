4a6561fdf39d39c34e59edb9d36a2977869390ea
sara <saramustafa1842002@gmail.com>
2023-11-09 09:48:57 -0800

    task done in c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,47 @@
-#include <stdlib h>
+#include <stddef.h>
 #include "lists.h"
+/**
+  * delete_dnodeint_at_index - hfffhh
+  * @head: bvccm
+  * @index: bggffh
+  * Return: bvcxx
+  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int count = 0;
+	dlisint_t *current;
+
 	if (*head == NULL)
 	{
-		   return -1; // List is empty, deletion failed
-			        }
+		return (-1);
+	}
+	current = *head;
 
-	        dlistint_t *current = *head;
-
-		    if (index == 0) {
-			            *head = current->next;
-				            if (*head != NULL) {
-						                (*head)->prev = NULL;
-								        }
-					            free(current);
-						            return 1; // Deletion succeeded
-							        }
-
-		        unsigned int count = 0;
-
-			    while (current != NULL && count < index) {
-				            current = current->next;
-					            count++;
-						        }
-
-			        if (current == NULL) {
-					        return -1; // Index is out of range, deletion failed
-						    }
-
-				    current->prev->next = current->next;
-
-				        if (current->next != NULL) {
-						        current->next->prev = current->prev;
-							    }
-
-					    free(current);
-					        return 1; // Deletion succeeded
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+	{
+		return (-1);
+	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
+	return (1);
 }
 
