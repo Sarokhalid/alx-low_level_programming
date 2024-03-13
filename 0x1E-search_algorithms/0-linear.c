74c64a5ef07e8a2bfae5940146d16b1e393b5d0c
sara <saramustafa1842002@gmail.com>
2024-03-13 04:17:16 +0000

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 size_t i;
 
 if (!array || size == 0)
-    return (-1);
+return (-1);
 for (i = 0; i < size; i++)
 {
     printf("Value checked array[%ld] = [%d]\n", i, array[i]);
